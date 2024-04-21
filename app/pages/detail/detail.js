9ef15baa4e982dc0458241780876d97b6fd3c9c4
sofiasg21 <sofiasanchezg21@gmail.com>
2024-04-21 19:20:50 +0200

    js commmit

---
+++ app/pages/detail/detail.js
@@ -0,0 +1,23 @@
+
+        let modal = document.getElementById("modal-detail");
+        
+      
+        function openModal(){
+            modal.style.display = "block";
+            console.log("modal abierto");
+        }
+        
+        function closeModal(){
+            modal.style.display = "none";
+            console.log("modal cerrado");
+        }
+        
+        
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+
+
+        
