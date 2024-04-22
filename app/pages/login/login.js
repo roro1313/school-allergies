96e1d89783c049529275d9b78edb27f5123b24c5
sofiasg21 <sofiasanchezg21@gmail.com>
2024-04-22 13:42:20 +0200

    otro commit login js

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -1,31 +1,30 @@
 const username = 'admin';
 const password = 'pass';
 
-const urlLogin = 'https://school-allergies.onrender.com/login';
+const url = 'https://school-allergies.onrender.com/login';
 
-const opcionesFetch = {
-    method: 'POST', // Método HTTP POST para enviar datos sensibles
-    headers: {
-      'Content-Type': 'application/json' // Especifica que los datos se envían en formato JSON
-    },
-    body: JSON.stringify(datosUsuario) // Convierte los datos del usuario a formato JSON
-  };
-  
-  // Realiza la solicitud fetch
-  fetch(urlLogin, opcionesFetch)
-    .then(response => {
-      // Maneja la respuesta de la solicitud
-      if (!response.error===false) {
-        console.log('Inicio de sesión exitoso');
-      } else {
-        throw new Error('Error en la solicitud de inicio de sesión');
-      }
-    })
-    .catch(error => {
-      // Captura cualquier error que ocurra durante la solicitud
-      console.error('Error:', error);
-    });
+const data = {
+  username: username,
+  password: password
+};
 
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+};
 
-
-
+fetch(urlLogin, options)
+  .then(response => {
+    if (response.!response.error===false)  {
+      console.log('Inicio de sesión exitoso');
+      // Realizar acciones adicionales después de iniciar sesión correctamente
+    } else {
+      console.error('Error en la solicitud de inicio de sesión');
+    }
+  })
+  .catch(error => {
+    console.error('Error en la solicitud:', error);
+  });
