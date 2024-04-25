600f8166337d92b0ce39e2dcfde6a010c48e74d6
sofiasg21 <sofiasanchezg21@gmail.com>
2024-04-25 09:59:19 +0200

    serv

--- app/pages/detail/detailuno.js
+++ app/pages/detail/detailuno.js
@@ -1,45 +1,83 @@
-document.getElementById('formularioBusqueda').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const nombreAlumno = document.getElementById('nombreAlumno').value;
-    buscarAlumno(nombreAlumno);
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get('userId');
+
+document.addEventListener("DOMContentLoaded", function() {
+    // When the page is loaded, call studentDetail() to get all student information
+    studentDetail();
 });
 
-function buscarAlumno(nombreAlumno) {
-    fetch('/ruta/hacia/tu/api/alumnos', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ nombre: nombreAlumno })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('La solicitud no se pudo completar.');
+// Calling endpoint with userId from url param
+async function studentDetail() {
+    const token = localStorage.getItem("token");
+  
+    try {
+      const response = await fetch(
+        "https://school-allergies.onrender.com/students",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ userId }),
         }
-        return response.json();
-    })
-    .then(data => {
-        mostrarInformacionAlumno(data);
-    })
-    .catch(error => {
-        console.error('Error al buscar el alumno:', error);
-        mostrarError('No se pudo encontrar la información del alumno.');
+      );
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        displayTables(data.response);
+      } else {
+        console.log(data.response); // Show error message if search fails
+      }
+    } catch (error) {
+      console.error(error);
+      console.error("An error occurred. Please try again later."); // Show error message
+    }
+  }
+
+  function displayTables(students) {
+    const tableContainer = document.getElementById("tableContainer");
+    tableContainer.innerHTML = "";
+    
+    students.forEach(student => {
+        // For each allergy print a table with allergy detail
+      const tableHTML = `
+        <h1>${student.studentName}'s allergies</h1>
+        ${student.allergies.map(allergy => `
+          <h3>${allergy.allergy} - ${allergy.medication}</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Crisis</th>
+                <th>Fecha</th>
+                <th>Información</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${allergy.crisis.map(crisis => `
+                <tr>
+                  <td>${crisis.type}</td>
+                  <td>${crisis.timestamp}</td>
+                  <td>${crisis.information}</td>
+                </tr>
+              `).join("")}
+            </tbody>
+          </table>
+        `).join("")}
+      `;
+      tableContainer.innerHTML += tableHTML;
     });
-}
-
-function mostrarInformacionAlumno(alumno) {
-    const resultadoBusqueda = document.getElementById('resultadoBusqueda');
-    resultadoBusqueda.innerHTML = `
-        <h2>Información del Alumno:</h2>
-        <p><strong>Alergia:</strong> ${alumno.alergia}</p>
-        <p><strong>Medicación:</strong> ${alumno.medicacion}</p>
-        <p><strong>Tipo de Crisis:</strong> ${alumno.tipoCrisis}</p>
-        <p><strong>Fecha:</strong> ${alumno.fecha}</p>
-        <p><strong>Información:</strong> ${alumno.informacion}</p>
-    `;
-}
-
-function mostrarError(mensaje) {
-    const resultadoBusqueda = document.getElementById('resultadoBusqueda');
-    resultadoBusqueda.innerHTML = `<p>${mensaje}</p>`;
-}
+  }
+  
+  function toggleMenu() {
+    const speedDial = document.getElementById("speedDial");
+    speedDial.classList.toggle("active");
+  }
+  // Manage clicks outside the plus menu
+  window.addEventListener("click", function (event) {
+    const speedDial = document.getElementById("speedDial");
+    if (!event.target.matches(".plus-menu")) {
+      speedDial.classList.remove("active");
+    }
+  });
