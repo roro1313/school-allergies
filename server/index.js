258490645b731026a362ed6e8c3022e201e0b1e0
Rocío Rodríguez <rociorgz@gmail.com>
2024-05-06 21:43:13 +0200

    add allergy deletion

--- server/index.js
+++ server/index.js
@@ -243,6 +243,37 @@ app.post(
     }
   }
 );
+app.delete(
+  "/students/delete-allergy",
+  authenticateToken(["admin"]),
+  async (req, res) => {
+    try {
+      const deletedAllergy = await db.collection("students").updateOne(
+        { userId: req.body.userId },
+        {
+          $pull: {
+            allergies: { allergy: req.body.allergy },
+          },
+        }
+      );
+      if (deletedAllergy.modifiedCount === 0) {
+        throw new Error("Allergy not found for userId " + req.body.userId + " and allergy " + req.body.allergy);
+      }
+
+      res.json({
+        error: false,
+        message: "Allergy deleted successfully",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error: true,
+        message: "Error deleting allergy",
+      });
+    }
+  }
+);
+
 // CRISIS
 app.post(
   "/students/new-crisis",
