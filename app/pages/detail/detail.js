81df3e6ddac12804dc66437275249395679b526a
Rocío Rodríguez <rociorgz@gmail.com>
2024-05-08 23:30:10 +0200

    fix detail page and tables

--- app/pages/detail/detail.js
+++ app/pages/detail/detail.js
@@ -41,13 +41,9 @@ async function studentDetail() {
     const data = await response.json();
 
     if (response.ok) {
-      // Store allergies to show them in a select
-      localStorage.setItem("select", data.response[0].allergies.map((allergy) => allergy.allergy))
-      // Show tables with allergies info
-      displayTables(data.response);
       // Show student info in sidebar
       const studentInfo = document.getElementById("studentInfo");
-      studentInfo.innerHTML= `
+      studentInfo.innerHTML = `
       <p class="info-label">Nombre:</p>
       <p class="info-detail">${data.response[0].studentName}</p>
       <p class="info-label">Apellido:</p>
@@ -57,17 +53,28 @@ async function studentDetail() {
       <p class="info-label">Curso:</p>
       <p class="info-detail">${data.response[0].studentGrade}</p>
       `;
+      // Store allergies to show them in a select
+      localStorage.setItem(
+        "select",
+        data.response[0].allergies.map((allergy) => allergy.allergy)
+      );
+      // Show tables with allergies info
+      displayTables(data.response);
+
     } else {
       console.log(data.response); // Show error message if search fails
+      const errorMessage = document.getElementById("errorMessage");
+      errorMessage.innerText = "Error: " + data.response;
     }
   } catch (error) {
     console.error(error);
-    console.error("An error occurred. Please try again later."); // Show error message
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerText = "An error occurred. Please try again later.";
   }
 }
 
 function displayTables(students) {
-  const tableContainer = document.getElementById("tableContainer");
+  const tableContainer = document.getElementById("tableInfoContainer");
   tableContainer.innerHTML = "";
 
   students.forEach((student) => {
@@ -75,7 +82,7 @@ function displayTables(students) {
     const tableHTML = `
         <h1>${student.studentName}'s allergies</h1>
         ${student.allergies
-          .map(
+         .map(
             (allergy) => `
           <h3>${allergy.allergy} - ${allergy.medication}</h3>
           <table>
@@ -87,23 +94,29 @@ function displayTables(students) {
               </tr>
             </thead>
             <tbody>
-              ${allergy.crisis
-                .map(
-                  (crisis) => `
-                <tr>
-                  <td>${crisis.type}</td>
-                  <td>${crisis.timestamp}</td>
-                  <td>${crisis.information}</td>
-                </tr>
-              `
-                )
-                .join("")}
+              ${
+                allergy.crisis.length === 0
+                 ? `<tr><td colspan="3">Esta alergia no tiene registradas crisis</td></tr>`
+                  : allergy.crisis
+                     .map(
+                        (crisis) => `
+                      <tr>
+                        <td>${crisis.type}</td>
+                        <td>${crisis.timestamp}</td>
+                        <td>${crisis.information}</td>
+                      </tr>
+                    `
+                      )
+                     .join("")
+              }
             </tbody>
           </table>
         `
           )
-          .join("")}
+         .join("")}
       `;
-    tableContainer.innerHTML += tableHTML;
+    const tableElement = document.createElement("div");
+    tableElement.innerHTML = tableHTML;
+    tableContainer.appendChild(tableElement);
   });
 }
